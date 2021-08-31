232b18b8a589be7adfb42a08fb2a3859a705ab98
Alexandru <>
2021-08-31 17:28:59 +0200

    simple-toDo-list

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
 import {
   StyleSheet,
-  Text,
   View,
-  TextInput,
-  Button,
-  ScrollView,
   FlatList
 } from 'react-native';
 
@@ -14,21 +10,44 @@ import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle }
     ]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  }
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAssMode(false);
   };
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title="Add New To Do" onPress={() => setIsAddMode(true)} />
+      <GoalInput 
+        visible={isAddMode} 
+        onAddGoal={addGoalHandler} 
+        onCancel={cancelGoalAdditionHandler}
+      />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
-        renderItem={itemData => <GoalItem title={itemData.item.value} />}
+        renderItem={
+        itemData => 
+          <GoalItem 
+            id={itemData.item.id} 
+            onDelete={removeGoalHandler} 
+            title={itemData.item.value} 
+          />}
       />
     </View>
   );
